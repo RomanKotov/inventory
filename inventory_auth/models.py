3c417aeb91c608c421d97afc90799ccc14955c60
Roman Kotov <RomanKotov@users.noreply.github.com>
2026-08-13 09:10:56 +0300

    Show custom user model in auth admin section

--- inventory_auth/models.py
+++ inventory_auth/models.py
@@ -1,5 +1,14 @@
 from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
 
 
 class User(AbstractUser):
     pass
+
+
+class UserAdminModelProxy(User):
+    class Meta:
+        proxy = True
+        app_label = "auth"
+        verbose_name = _("user")
+        verbose_name_plural = _("users")
